13c6c43311601f0a19f1414a6c867477fcd8d719
Valerio Luzzi <valluzzi@gmail.com>
2022-11-25 16:18:18 +0100

    rasterlike

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.0.61"
+VERSION = "0.0.62"
 
 PACKAGE_NAME = "gdal2numpy"
 AUTHOR = "Valerio Luzzi"
--- src/gdal2numpy/__init__.py
+++ src/gdal2numpy/__init__.py
@@ -25,6 +25,6 @@
 from .module_GDAL2Numpy import *
 from .module_Numpy2GTiff import *
 from .module_MetaData import *
-from .module_ogr import SetGDALEnv
+from .module_ogr import *
 from .rasterlike import RasterLike
 from .gdalwarp import gdalwarp
