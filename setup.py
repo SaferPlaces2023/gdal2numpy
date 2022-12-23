e0f55dc1b9b70f3702213a67a9e951ca6086a582
Valerio Luzzi <valluzzi@gmail.com>
2022-12-23 09:27:38 +0100

    fix SetTag

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.0.72"
+VERSION = "0.0.73"
 
 PACKAGE_NAME = "gdal2numpy"
 AUTHOR = "Valerio Luzzi"
--- src/gdal2numpy/module_MetaData.py
+++ src/gdal2numpy/module_MetaData.py
@@ -153,7 +153,8 @@ def SetTag(filename, tagname, tagvalue="", band=0):
     """
     SetTag - set a tag in metadata of the file or of the band if specified
     """
-    print("0) SetTag...")
+    print("0) SetTag...". filename, isshape(filename), os.path.isfile(filename))
+    print("============================")
     if israster(filename):
 
         ds = gdal.Open(filename, gdalconst.GA_Update)
