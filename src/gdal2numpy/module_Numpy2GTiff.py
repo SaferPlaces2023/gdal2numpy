91b2aa98686dbd900eeaea68751d3a5f20aac8cc
valluzzi <valluzzi@gmail.com>
2023-04-19 09:14:11 +0200

    SetStatistics

--- src/gdal2numpy/module_Numpy2GTiff.py
+++ src/gdal2numpy/module_Numpy2GTiff.py
@@ -143,7 +143,8 @@ def Numpy2GTiff(arr, gt, prj, fileout, format="GTiff", save_nodata_as=-9999, met
                 meanValue = np.nanmean(arr)
                 stdValue = np.nanstd(arr)
                 print("Statistics: ", minValue, maxValue, meanValue, stdValue)
-                ds.GetRasterBand(1).SetStatistics(minValue, maxValue, meanValue, stdValue)
+                print("============================================================")
+                #ds.GetRasterBand(1).SetStatistics(minValue, maxValue, meanValue, stdValue)
             # ---
 
             ds.GetRasterBand(1).SetNoDataValue(save_nodata_as)
