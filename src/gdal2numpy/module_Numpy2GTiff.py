27449197c69659d31f255c93b62d15aa2eaed33e
valluzzi <valluzzi@gmail.com>
2023-05-22 19:13:03 +0200

    warning

--- src/gdal2numpy/module_Numpy2GTiff.py
+++ src/gdal2numpy/module_Numpy2GTiff.py
@@ -144,8 +144,8 @@ def Numpy2GTiff(arr, gt, prj, fileout, format="GTiff", save_nodata_as=-9999, met
                 maxValue = float(np.nanmax(arr))
                 meanValue = float(np.nanmean(arr))
                 stdValue = float(np.nanstd(arr))
-                print("Statistics: ", minValue, maxValue, meanValue, stdValue, dtype)
-                print("============================================================")
+                # print("Statistics: ", minValue, maxValue, meanValue, stdValue, dtype)
+                # print("============================================================")
                 ds.GetRasterBand(1).SetStatistics(minValue, maxValue, meanValue, stdValue)
             # ---
 
