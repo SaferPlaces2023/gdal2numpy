7b593565cfeff3ae9f50a9554a37c141311ad392
valluzzi <valluzzi@gmail.com>
2023-11-23 16:44:51 +0100

    debug OpenShape

--- src/gdal2numpy/module_open.py
+++ src/gdal2numpy/module_open.py
@@ -35,21 +35,21 @@ def OpenShape(fileshp, exclusive=False):
     if not fileshp:
         ds = None
     elif isinstance(fileshp, str) and isshape(fileshp):
-        Logger.debug(f"Opening {fileshp}...")
+        print(f"1) Opening {fileshp}...")
         ds = ogr.Open(fileshp, exclusive)
     elif isinstance(fileshp, str) and iss3(fileshp) and fileshp.endswith(".shp"):
-        Logger.debug(f"Downloading file from s3...")
+        print(f"2) Downloading file from s3...")
         #ds = ogr.Open(copy(fileshp), exclusive)
         fileshp = fileshp.replace("s3://", "/vsis3/")
         ds = ogr.Open(fileshp, exclusive)
     elif isinstance(fileshp, str) and fileshp.startswith("http") and fileshp.endswith(".shp"):
-        Logger.debug(f"Inspect file from https...")
+        print(f"3) Inspect file from https...")
         ds = ogr.Open(f"/vsicurl/{fileshp}", exclusive)
     elif isinstance(fileshp, ogr.DataSource) and GetAccess(fileshp) >= exclusive:
-        Logger.debug(f"Dataset already open...")
+        print(f"4) Dataset already open...")
         ds = fileshp
     elif isinstance(fileshp, ogr.DataSource) and GetAccess(fileshp) < exclusive:
-        Logger.debug(f"Change the open mode: Open({exclusive})")
+        print(f"5) Change the open mode: Open({exclusive})")
         ds = ogr.Open(fileshp.GetName(), exclusive)
     else:
         ds = None
