69ca09cd6a3f1d5269faf581a7a2d6bfc23c361e
valluzzi <valluzzi@gmail.com>
2023-12-22 16:38:49 +0100

    gdalwarp improved

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.0.266"
+VERSION = "0.0.267"
 PACKAGE_NAME = "gdal2numpy"
 AUTHOR = "Valerio Luzzi, Marco Renzi"
 EMAIL = "valerio.luzzi@gecosistema.com, marco.renzi@gecosistema.com"
