c170cf52a1284049b79b88a7dca8c0d47a76a1a4
valluzzi <valluzzi@gmail.com>
2023-12-22 12:34:11 +0100

    fix gdalwarp cog

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.0.261"
+VERSION = "0.0.262"
 PACKAGE_NAME = "gdal2numpy"
 AUTHOR = "Valerio Luzzi, Marco Renzi"
 EMAIL = "valerio.luzzi@gecosistema.com, marco.renzi@gecosistema.com"
