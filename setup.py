e41dec4007bb9773cc8166462c13f4206693e9f3
valluzzi <valluzzi@gmail.com>
2024-02-14 10:44:23 +0100

    gdalwarp cutline

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.0.271"
+VERSION = "0.0.272"
 PACKAGE_NAME = "gdal2numpy"
 AUTHOR = "Valerio Luzzi, Marco Renzi"
 EMAIL = "valerio.luzzi@gecosistema.com, marco.renzi@gecosistema.com"
