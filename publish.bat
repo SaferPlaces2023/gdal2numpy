7050d3db29ce80d6364656d20a9bee298127c732
valluzzi <valluzzi@gmail.com>
2024-02-14 18:51:14 +0100

    0.0.282

--- publish.bat
+++ publish.bat
@@ -1,9 +1,32 @@
 @echo off
-set comment=%1
+
+:: find the version number in package.json and increment it
+for /F "tokens=2 delims==, " %%t in ('findstr /C:"VERSION " setup.py') do (
+    set v=%%~t
+    for /F "tokens=3 delims=." %%t in ('echo %%~t') do (
+        set v=%%~t
+        set /A v+=1
+    )
+)
+set version="0.0.%v%"
+
+
+:: replace the version number in package.json
+echo import setuptools>setup.bkp
+echo.>>setup.bkp
+echo VERSION = %version%>>setup.bkp
+:: copy the file from line 3 to the end of the file avoid last line
+type setup.py | more +3 >>setup.bkp
+
+
+:: write the version file
+
+::commit and push the changes
+set comment=%version%
 cmd /c git add .
 cmd /c git commit -m %comment%
 cmd /c git push
-cmd /c rmdir /Q /S build
-cmd /c rmdir /Q /S dist
-cmd /c python setup.py sdist
-cmd /c twine upload dist/*
+::finally publish the package on npm
+cmd /c npm run build 
+cmd /c npm publish 
+
--- setup.bkp
+++ setup.bkp
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.0.281"
+VERSION = "0.0.282"
 PACKAGE_NAME = "gdal2numpy"
 AUTHOR = "Valerio Luzzi, Marco Renzi"
 EMAIL = "valerio.luzzi@gecosistema.com, marco.renzi@gecosistema.com"
