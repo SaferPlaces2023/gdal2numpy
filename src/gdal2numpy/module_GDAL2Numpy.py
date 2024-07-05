b8dd6fad5f7593aa72876a33e91220d2e804dec9
valluzzi <valluzzi@gmail.com>
2024-07-05 12:20:08 +0200

    v 0.0.302

--- src/gdal2numpy/module_GDAL2Numpy.py
+++ src/gdal2numpy/module_GDAL2Numpy.py
@@ -66,6 +66,9 @@ def GDAL2Numpy(filename, band=1, dtype=np.float32, load_nodata_as=np.nan, bbox=[
             j0, i0 = int((X0 - x0) / px), int((Y1 - y0) / py)
             cols, rows = math.ceil((X1 - X0) / px), math.ceil(abs(Y1 - Y0) / abs(py))
 
+            print("mxn", m,"x", n)
+            print("cols, rows", cols, "x", rows)    
+
             # index-safe
             j0, i0 = min(max(j0, 0), n - 1), min(max(i0, 0), m - 1)
             cols = min(max(cols, 0), n)
