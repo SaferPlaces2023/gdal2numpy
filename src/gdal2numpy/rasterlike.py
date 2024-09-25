d5d4b21702e25d4cfdb33d1cb729ec512a2567ee
valluzzi <valluzzi@gmail.com>
2024-09-25 10:40:55 +0200

    v 0.0.343

--- src/gdal2numpy/rasterlike.py
+++ src/gdal2numpy/rasterlike.py
@@ -25,20 +25,23 @@
 import os
 import numpy as np
 from .filesystem import tempfilename
-from .module_ogr import SamePixelSize, SameSpatialRef, GetSpatialRef, GetExtent, SameExtent, GetPixelSize, ogr_remove, \
-    Rectangle, CreateRectangleShape
+from .module_ogr import SamePixelSize, SameSpatialRef, GetSpatialRef, GetExtent, SameExtent
+from .module_ogr import ogr_remove, Rectangle, CreateRectangleShape, GetPixelSize
 from .module_GDAL2Numpy import GDAL2Numpy
 from .module_Numpy2GTiff import Numpy2GTiff
 from .gdalwarp import gdalwarp
 from .module_log import Logger
+from .module_s3 import copy
 
 
 def RasterLike(filetif, filetpl, fileout=None, resampleAlg="near", format="GTiff", verbose=False):
     """
-    RasterLike: adatta un raster al raster template ( dem ) ricampionando, riproiettando estendendo/clippando il file raster se necessario.
+    RasterLike: adatta un raster al raster template ( dem ) ricampionando, 
+    riproiettando estendendo/clippando il file raster se necessario.
     """
+    Logger.debug("0)RasterLike...")
     fileout = fileout if fileout else tempfilename(suffix=".tif")
-    
+
     if SameSpatialRef(filetif, filetpl) and \
         SamePixelSize(filetif, filetpl, decimals=2) and \
             SameExtent(filetif, filetpl, decimals=3):
@@ -46,12 +49,39 @@ def RasterLike(filetif, filetpl, fileout=None, resampleAlg="near", format="GTiff
         fileout = filetif
         return fileout
 
-    Logger.debug("1)gdalwarp...")
-    file_warp1 = tempfilename(suffix=".warp1.tif")
-    file_warp1 = gdalwarp([filetif], fileout=file_warp1, dstSRS=GetSpatialRef(filetpl),
-                          pixelsize=GetPixelSize(filetpl, um=None),
-                          resampleAlg=resampleAlg, format=format)
+    srs_tpl = GetSpatialRef(filetpl)
+    # Tiff extent
+    tif_minx, tif_miny, tif_maxx, tif_maxy = GetExtent(filetif, srs_tpl)
+    # Template extent
+    tpl_minx, tpl_miny, tpl_maxx, tpl_maxy = GetExtent(filetpl)
+    # Template extent 10% larger
+    delta = 0.1
+    crp_minx, crp_miny, crp_maxx, crp_maxy = tpl_minx - delta * (tpl_maxx - tpl_minx), tpl_miny - delta * (
+        tpl_maxy - tpl_miny), tpl_maxx + delta * (tpl_maxx - tpl_minx), tpl_maxy + delta * (tpl_maxy - tpl_miny)
 
+    tif_rectangle = Rectangle(tif_minx, tif_miny, tif_maxx, tif_maxy)
+    tpl_rectangle = Rectangle(tpl_minx, tpl_miny, tpl_maxx, tpl_maxy)
+    # first coarse crop to speed up the process
+    if tif_rectangle.Intersects(tpl_rectangle) and \
+        tif_rectangle.GetArea()/tpl_rectangle.GetArea() > 4:
+            file_rect = tempfilename(suffix=".shp")
+            cropshape = CreateRectangleShape(crp_minx, crp_miny, crp_maxx, crp_maxy,
+                                            srs=srs_tpl,
+                                            fileshp=file_rect)
+            file_warp0 = gdalwarp([filetif], cutline=cropshape,
+                                cropToCutline=False, dstSRS=srs_tpl)
+            ogr_remove(cropshape)
+            Logger.debug(f"0e)gdalwarp...done {file_warp0}")
+    else:
+        file_warp0 = copy(filetif)
+
+    Logger.debug("1)gdalwarp...")
+    # Second warp to the template SRS and pixel size
+    file_warp1 = tempfilename(suffix=".warp1.tif")
+    file_warp1 = gdalwarp([file_warp0], fileout=file_warp1, dstSRS=srs_tpl,
+                          pixelsize=GetPixelSize(filetpl, um=None),
+                          resampleAlg=resampleAlg)
+    Logger.debug(f"1a)gdalwarp...done {file_warp1}")
     tif_minx, tif_miny, tif_maxx, tif_maxy = GetExtent(file_warp1)
     tpl_minx, tpl_miny, tpl_maxx, tpl_maxy = GetExtent(filetpl)
     # create tif and template rectangles
@@ -59,24 +89,25 @@ def RasterLike(filetif, filetpl, fileout=None, resampleAlg="near", format="GTiff
     tif_rectangle = Rectangle(tif_minx, tif_miny, tif_maxx, tif_maxy)
     tpl_rectangle = Rectangle(tpl_minx, tpl_miny, tpl_maxx, tpl_maxy)
 
-    Logger.debug("rectangle done")
+    Logger.debug("1b)rectangle done")
+    # Final Refinement of the crop
     if tif_rectangle.Intersects(tpl_rectangle):
-        Logger.debug("intersection")
         file_rect = tempfilename(suffix=".shp")
-        spatialRefSys = GetSpatialRef(filetpl)
         demshape = CreateRectangleShape(tpl_minx, tpl_miny, tpl_maxx, tpl_maxy,
-                                        srs=spatialRefSys,
+                                        srs=srs_tpl,
                                         fileshp=file_rect)
-        Logger.debug("2)gdalwarp...")
+        Logger.debug("2b)gdalwarp...")
         gdalwarp([file_warp1], fileout, cutline=demshape, cropToCutline=True,
-                 dstSRS=GetSpatialRef(filetpl), pixelsize=GetPixelSize(filetpl, um=None), resampleAlg=resampleAlg, format=format)
-
+                 dstSRS=srs_tpl, pixelsize=GetPixelSize(filetpl, um=None), resampleAlg=resampleAlg, format=format)
+        Logger.debug(f"2c)gdalwarp...done {fileout}")
         ogr_remove(file_rect)
 
     else:
-        wdata, gt, prj = GDAL2Numpy(filetpl, band=1, dtype=np.float32, load_nodata_as=np.nan)
+        wdata, gt, prj = GDAL2Numpy(
+            filetpl, band=1, dtype=np.float32, load_nodata_as=np.nan)
         wdata.fill(np.nan)
         Numpy2GTiff(wdata, gt, prj, fileout, format=format)
 
+    os.unlink(file_warp0)
     os.unlink(file_warp1)
     return fileout if os.path.exists(fileout) else None
