5375e4fd01984ba39ee9dd7b28f8e7545d72e04b
valluzzi <valluzzi@gmail.com>
2024-10-10 16:39:29 +0200

    fix action

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.0.397"
+VERSION = "0.0.398"
 PACKAGE_NAME = "gdal2numpy"
 AUTHOR = "Valerio Luzzi, Marco Renzi"
 EMAIL = "valerio.luzzi@gecosistema.com, marco.renzi@gecosistema.com"
