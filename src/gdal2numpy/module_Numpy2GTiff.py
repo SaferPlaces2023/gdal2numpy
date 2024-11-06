8f37d5d30aa343d456e4f6e8ebcca7352f919dc0
valluzzi <valluzzi@gmail.com>
2024-11-06 11:37:42 +0100

    fix stats on Numpy2GTIff

--- src/gdal2numpy/module_Numpy2GTiff.py
+++ src/gdal2numpy/module_Numpy2GTiff.py
@@ -74,10 +74,13 @@ def CalculateStats(ds):
     arr = ds.GetRasterBand(1).ReadAsArray()
     if dtype in (gdal.GDT_Float32, gdal.GDT_Float64):
         arr[arr <= -9999] = np.nan
-        minValue = float(np.nanmin(arr))
-        maxValue = float(np.nanmax(arr))
-        meanValue = float(np.nanmean(arr))
-        stdValue = float(np.nanstd(arr))
+        if np.any(~np.isnan(arr)):
+            minValue = float(np.nanmin(arr))
+            maxValue = float(np.nanmax(arr))
+            meanValue = float(np.nanmean(arr))
+            stdValue = float(np.nanstd(arr))
+        else:
+            minValue = maxValue = meanValue = stdValue = 0
         ds.GetRasterBand(1).SetStatistics(minValue, maxValue, meanValue, stdValue)
 
 
